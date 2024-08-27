9d12f71bfb043c9879a2a39575d4f88dfcde08ef
Ali raza <malirazaansari4@gmail.com>
2024-08-27 16:18:57 +0500

    2nd problem done reverse text or sentence

--- reverse_words.js
+++ reverse_words.js
@@ -1,17 +1,17 @@
-function reverseString(s) {
-  return s.split("").reverse().join("");
-}
+// function reverseString(s) {
+//   return s.split("").reverse().join("");
+// }
 
-console.log(reverseString("hello"));
+// console.log(reverseString("hello"));
 
-function reverseWords(d) {
-  let str = d.split(" ");
-  str.reverse();
-  return str.join(" ");
-}
+// function reverseWords(d) {
+//   let str = d.split(" ");
+//   str.reverse();
+//   return str.join(" ");
+// }
 
-let d = "geeks for all";
-console.log(reverseWords(d));
+// let d = "geeks for all";
+// console.log(reverseWords(d));
 
 function isPalindrome(s) {
   const cleanedStr = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
@@ -28,11 +28,11 @@ function areAnagrams(s1, s2) {
 // console.log(areAnagrams("listen", "silent"));  // Output: true
 console.log(areAnagrams("ali", "ila"));
 
-function abc(s) {
-  let str = s;
-  str = str.split("").reverse().join("");
-  return str;
-}
+// function abc(s) {
+//   let str = s;
+//   str = str.split("").reverse().join("");
+//   return str;
+// }
 
-const reversestr = abc("Ansari");
-console.log(reversestr);
+// const reversestr = abc("Ansari");
+// console.log(reversestr);
