f0845a0e212dbea19d7097c8df6b550fcfc0ff87
Ali raza <malirazaansari4@gmail.com>
2024-10-05 14:21:48 +0500

    commit 3

--- array_questions.js
+++ array_questions.js
@@ -44,21 +44,3 @@ console.log(twoSum([2, 7, 11, 15], 26));
 // console.log(string);
 
 // let rows = 5;
-
-// // pattern variable carries the final pattern in string format
-// let pattern = "";
-
-// // outer loop runs for `rows` no. of times
-// for (let n = 1; n <= rows; n++) {
-//   // Inner Loop - I -> prints spaces
-//   for (let space = 1; space <= rows - n; space++) {
-//     pattern += " ";
-//   }
-
-//   // Inner Loop - II -> prints stars
-//   for (let num = 1; num <= 2 * n - 1; num++) {
-//     pattern += "*";
-//   }
-//   pattern += "\n";
-// }
-// console.log(pattern);
